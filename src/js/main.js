6226b6889da424d47979d584ed4f0988377a1d77
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-06-02 21:23:39 +0200

    added the alert page to the html layout

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+
+const main = (() => {
+const btn = document.querySelector("#btn_popup");
+
+//function to open and close the alert page
+const alertMenu = () => {
+  console.log("alertMenu");
+}
+
+btn.addEventListener("click", alertMenu)
+
+
+})()
