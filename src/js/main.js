005d5d9dcb30051dce1fd3bb7b622ad38186ad82
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-06-02 22:48:46 +0200

    wrote the entore js for the app

--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,23 @@ const main = (() => {
   const btn = document.querySelector("#btn_popup");
   const close = document.querySelector(".x-icon");
   const alert = document.querySelector(".page_alert");
+  const alert_p = document.querySelector(".page_alert_box_p");
+
+  var storeAmount = 0;
 
   //function to open the alert page
   const openAlertMenu = () => {
+    //add a style to the alert display
     alert.style.display = "flex";
-    console.log("alertMenu");
+    //increment the amount
+    storeAmount += 1;
+    //add the amount of clicks to the alert text
+    alert_p.textContent = `You have clicked this button ${storeAmount} times`;
   };
 
   //function to close the alert page
   const closeAlertMenu = () => {
     alert.style.display = "none";
-    console.log("alertMenu");
   };
 
   //added click event to the button to open alert
